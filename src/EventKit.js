7f12a1c21d225314fa6c9414406f5b63d2c472e7
steelbrain <aneesiqbalbhatti@gmail.com>
2015-07-26 04:37:45 -0600

    :bug: Fix the browser build

--- src/EventKit.js
+++ src/EventKit.js
@@ -1,5 +1,8 @@
-module.exports = {
+let EventKit = {
   CompositeDisposable: require('./CompositeDisposable'),
   Disposable: require('./Disposable'),
   Emitter: require('./Emitter')
 }
+if(typeof window !== 'undefined'){
+  window.EventKit = EventKit
+} else module.exports = EventKit
