7a7af38926b4ea65f5c602627db812a7d0a3d8f1
steelbrain <aneesiqbalbhatti@gmail.com>
2015-07-26 03:51:41 -0600

    :new: Add CompositeDisposable class

---
+++ Src/Composite-Disposible.js
@@ -0,0 +1,25 @@
+class CompositeDisposable{
+  disposed = false
+  constructor(){
+    this.disposables = new Set(arguments)
+  }
+  dispose(){
+    if(this.disposed) return
+    this.disposed = true
+    this.disposables.forEach(item => item.dispose())
+    this.disposables = null
+  }
+  add(){
+    if(this.disposed) return
+    Array.prototype.forEach.call(arguments, item => this.disposables.add(item))
+  }
+  remove(){
+    if(this.disposed) return
+    Array.prototype.forEach.call(arguments, item => this.disposables.delete(item))
+  }
+  clear(){
+    if(this.disposed) return
+    this.disposables.clear()
+  }
+}
+module.exports = CompositeDisposable
