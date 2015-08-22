22429bddbc4472900aa772d995535b71b8566b36
steelbrain <aneesiqbalbhatti@gmail.com>
2015-08-22 14:25:00 -0600

    :bug: Add web worker support

--- src/Browser.js
+++ src/Browser.js
@@ -1,6 +1,15 @@
-window.EventKit = window.EventKit || {
-  CompositeDisposable: require('./CompositeDisposable'),
-  Disposable: require('./Disposable'),
-  Emitter: require('./Emitter')
+if (typeof window !== 'undefined') {
+  window.EventKit = window.EventKit || {
+      CompositeDisposable: require('./CompositeDisposable'),
+      Disposable: require('./Disposable'),
+      Emitter: require('./Emitter')
+    }
+  module.exports = window.EventKit
+} else if (typeof self !== 'undefined') {
+  self.EventKit = self.EventKit || {
+      CompositeDisposable: require('./CompositeDisposable'),
+      Disposable: require('./Disposable'),
+      Emitter: require('./Emitter')
+    }
+  module.exports = self.EventKit
 }
-module.exports = window.EventKit
