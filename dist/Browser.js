d40081b6723e69a45eb5f14ae26752216f6b9aa2
steelbrain <aneesiqbalbhatti@gmail.com>
2015-08-22 14:25:11 -0600

    :arrow_up: Bump version to 1.0.2

--- dist/Browser.js
+++ dist/Browser.js
@@ -1,8 +1,17 @@
 'use strict';
 
-window.EventKit = window.EventKit || {
-  CompositeDisposable: require('./CompositeDisposable'),
-  Disposable: require('./Disposable'),
-  Emitter: require('./Emitter')
-};
-module.exports = window.EventKit;
+if (typeof window !== 'undefined') {
+  window.EventKit = window.EventKit || {
+    CompositeDisposable: require('./CompositeDisposable'),
+    Disposable: require('./Disposable'),
+    Emitter: require('./Emitter')
+  };
+  module.exports = window.EventKit;
+} else if (typeof self !== 'undefined') {
+  self.EventKit = self.EventKit || {
+    CompositeDisposable: require('./CompositeDisposable'),
+    Disposable: require('./Disposable'),
+    Emitter: require('./Emitter')
+  };
+  module.exports = self.EventKit;
+}
