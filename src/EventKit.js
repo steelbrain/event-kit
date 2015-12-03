d77abfc8a5573c2117a36456f573a92f9716e4c6
steelbrain <aneesiqbalbhatti@gmail.com>
2015-12-03 04:35:39 -0700

    :fire: Remove some unnecessary files

--- src/EventKit.js
+++
@@ -1,7 +0,0 @@
-// @Compiler-Babel "true"
-// @Compiler-Output "../dist/EventKit.js"
-module.exports = {
-  CompositeDisposable: require('./CompositeDisposable'),
-  Disposable: require('./Disposable'),
-  Emitter: require('./Emitter')
-}
