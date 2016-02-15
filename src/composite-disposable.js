a27ceb2b6ae838f905019abb1820714fabf5a0ca
steelbrain <de.steelbrain@gmail.com>
2016-02-15 06:40:37 +0000

    :new: Add isDisposed to CompositeDisposable

--- src/composite-disposable.js
+++ src/composite-disposable.js
@@ -1,9 +1,40 @@
 'use babel'
 
+/* @flow */
+
+import type {Disposable} from './disposable'
+
 export class CompositeDisposable {
+  disposed: boolean;
+  disposables: Set<Disposable>;
+
   constructor(){
     this.disposed = false
     this.disposables = new Set(arguments)
+  }
+  add(){
+    if (!this.disposed) {
+      let length = arguments.length
+      for (let i = 0; i < length; ++i) {
+        this.disposables.add(arguments[i])
+      }
+    }
+  }
+  remove(){
+    if (!this.disposed) {
+      let length = arguments.length
+      for (let i = 0; i < length; ++i) {
+        this.disposables.delete(arguments[i])
+      }
+    }
+  }
+  clear(){
+    if (!this.disposed) {
+      this.disposables.clear()
+    }
+  }
+  isDisposed(): boolean {
+    return this.disposed
   }
   dispose(){
     if (!this.disposed) {
@@ -11,21 +42,6 @@ export class CompositeDisposable {
       this.disposables.forEach(function(item) {
         item.dispose()
       })
-      this.disposables = null
-    }
-  }
-  add(){
-    if (!this.disposed) {
-      Array.prototype.forEach.call(arguments, item => this.disposables.add(item))
-    }
-  }
-  remove(){
-    if (!this.disposed) {
-      Array.prototype.forEach.call(arguments, item => this.disposables.delete(item))
-    }
-  }
-  clear(){
-    if (!this.disposed) {
       this.disposables.clear()
     }
   }
